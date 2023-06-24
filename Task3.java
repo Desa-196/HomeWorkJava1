b71361d04d6f51bc9f1abd6d625711b680a80973
Денис Пономарев <deskin196@gmail.com>
2023-06-24 18:04:51 +0300

    Написал функцию ввода оператора для вычисления

--- Task3.java
+++ Task3.java
@@ -1,6 +1,39 @@
 // 3) Реализовать простой калькулятор
+
+import java.util.Scanner;
+
 public class Task3{
     public static void main(String[] args) {
+        //float a = readIntFromConsole("Введите первое число: ");
+        String operator = readOperatorFromConsole();
+        //float b = readIntFromConsole("Введите второе число: ");
 
+    }
+
+    //Функция ввода  числа с проверкой
+    public static float readIntFromConsole(String text)
+    {
+        System.out.print(text);
+        Scanner input = new Scanner(System.in);
+        while(!input.hasNextFloat())
+        {
+            System.out.println("\nОшибка ввода, повторите ввод: ");
+            input.next();
+        }
+        float result = input.nextFloat();
+        return result;
+
+    }
+
+    public static String readOperatorFromConsole() {
+        System.out.print("Введите оператор выражения(/*+-): ");
+        Scanner input = new Scanner(System.in);
+        String operator = input.nextLine();
+        while( !(operator.equals("+") || operator.equals("-") || operator.equals("*") ||operator.equals("/")) )
+        {
+            System.out.print("Ошибка ввода, повторите: ");
+            operator = input.nextLine();
+        }
+        return operator;
     }
 }
