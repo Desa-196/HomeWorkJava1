0c61ddaf7d369ab820ee69ecf5b0b5c7c3d3914a
Денис Пономарев <deskin196@gmail.com>
2023-06-24 13:27:41 +0300

    Создал файл для решения задачи 2

---
+++ Task2.java
@@ -0,0 +1,11 @@
+// 2) Вывести все простые числа от 1 до 1000
+
+public class Task2{
+    public static void main(String[] args) {
+        
+    }
+    public static boolean isPrime(int n)
+    {
+
+    }
+}
