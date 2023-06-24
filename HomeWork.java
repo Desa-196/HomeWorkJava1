e720700a09f80ae4da379bd7f10714d6493eeb74
Денис Пономарев <deskin196@gmail.com>
2023-06-24 12:22:48 +0300

    Написал функцию ввода целого числа из консоли

--- HomeWork.java
+++ HomeWork.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class HomeWork{
     public static void main(String[] args) {
-        System.out.println(sum());
+        System.out.println(readIntFromConsole("Введите число N: "));
     }
     
     // 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
-    public static int sum() {
-        return 1;
+    //public static int[] sumAndMultiplication(int n) {
+     //   return new int {1,1};
+    //}
+    //Функция ввода целого числа с проверкой
+    public static int readIntFromConsole(String text)
+    {
+        System.out.print(text);
+        Scanner input = new Scanner(System.in);
+        while(!input.hasNextInt())
+        {
+            System.out.print("Ошибка ввода, повторите ввод: ");
+            input.next();
+        }
+        int result = input.nextInt();
+        input.close();
+        return result;
+
     }
 }
