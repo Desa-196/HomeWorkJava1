328534ee4256b496a5731636574fddbd031d90ac
Денис Пономарев <deskin196@gmail.com>
2023-06-24 12:08:33 +0300

    Создал файл для решения домашней работы

---
+++ HomeWork.java
@@ -0,0 +1,10 @@
+public class HomeWork{
+    public static void main(String[] args) {
+        System.out.println(sum());
+    }
+    
+    // 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+    public static int sum() {
+        return 1;
+    }
+}
