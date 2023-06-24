acb576f43dd441a8fcfe03207e686808e8211e30
Денис Пономарев <deskin196@gmail.com>
2023-06-24 13:33:59 +0300

    Написал алгоритм по задаче 2

--- Task2.java
+++ Task2.java
@@ -2,10 +2,22 @@
 
 public class Task2{
     public static void main(String[] args) {
+        //Проходимся по всем числам от 2 до 1000 и выводим только простые
+        for(int i = 2; i<=1000; i++)
+        {
+            if(isPrime(i)) System.out.println(i);
+        }
         
     }
+    //Функция проверяет, является ли число простым
     public static boolean isPrime(int n)
     {
+        if(n <= 1) return false;
 
+        for(int i = n - 1; i > 1; i--)
+        {
+            if(n % i == 0) return false;
+        }
+        return true;
     }
 }
